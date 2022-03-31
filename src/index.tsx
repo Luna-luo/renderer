532f09623d954429dff1e12cbf41e7ce27208248
Luna-luo <56943859+Luna-luo@users.noreply.github.com>
2022-03-31 15:09:55 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { FramedDocumentRenderer, fullAttachmentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
 
+console.log('aa')
+
 ReactDOM.render(
   <FramedDocumentRenderer templateRegistry={registry} attachmentToComponent={fullAttachmentRenderer} />,
   document.getElementById("root")
